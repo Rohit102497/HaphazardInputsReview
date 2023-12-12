4de4029135ce1e3bf8a906b95c6a586f1d0c09d7
Rohit Agarwal <rag004@vs-c1.cs.uit.no>
2023-12-12 07:33:59 +0100

    Added folders

---
+++ main.py
@@ -0,0 +1,22 @@
+#--------------Libraries--------------#
+
+
+#--------------Import Functions--------------#
+
+
+#--------------All Variables--------------#
+
+
+#--------------Load Data wrt Variables--------------#
+
+
+#--------------Load Method wrt Variables--------------#
+
+
+#--------------Run Model--------------#
+
+
+#--------------Calculate all Metrics--------------#
+
+
+#--------------Store results and all variables--------------#
